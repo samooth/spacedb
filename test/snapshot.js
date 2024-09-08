83dca78a34a8fe7476d71473a72aba3db8171d50
Mathias Buus <mathiasbuus@gmail.com>
2024-09-08 10:57:22 +0200

    test snap of snaps

--- test/snapshot.js
+++ test/snapshot.js
@@ -45,3 +45,33 @@ test('basic snapshot', async function (t) {
 
   await snap.close()
 })
+
+test('snap of snap', async function (t) {
+  const db = await rocks(t)
+
+  await db.insert('@db/members', { id: 'someone', age: 40 })
+  await db.insert('@db/members', { id: 'else', age: 50 })
+
+  const snap = db.snapshot()
+  const snapOfSnap = snap.snapshot()
+
+  await db.insert('@db/members', { id: 'baby', age: 1 })
+
+  t.alike(await snap.find('@db/members').toArray(), [{ id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+  t.alike(await snapOfSnap.find('@db/members').toArray(), [{ id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+
+  await snapOfSnap.close()
+
+  t.alike(await snap.find('@db/members').toArray(), [{ id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+
+  await db.flush()
+
+  t.alike(await snap.find('@db/members').toArray(), [{ id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'baby', age: 1 }, { id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+
+  await snap.close()
+
+  t.alike(await db.find('@db/members').toArray(), [{ id: 'baby', age: 1 }, { id: 'else', age: 50 }, { id: 'someone', age: 40 }])
+
+  await db.close()
+})
