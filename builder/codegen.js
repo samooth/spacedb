da7f904fa2df5b693bbead8cb0da2c4032f95ffb
Mathias Buus <mathiasbuus@gmail.com>
2024-09-09 11:32:41 +0200

    fix schema require in builder

--- builder/codegen.js
+++ builder/codegen.js
@@ -25,6 +25,7 @@ const type = contract.resolveIndex('@keet/devices-by-name')
 
 const gen = require('generate-object-property')
 const s = require('generate-string')
+const path = require('path')
 
 const IndexTypeMap = new Map([
   ['uint', 'IndexEncoder.UINT'],
@@ -53,7 +54,7 @@ module.exports = function generateCode (hyperdb) {
   str += '\n'
   str += 'const { IndexEncoder, c } = require(\'hyperdb/runtime\')\n'
   str += '\n'
-  str += 'const { version, resolveStruct } = require(\'./messages.js\')\n'
+  str += `const { version, resolveStruct } = require('${generateRequire(hyperdb.dir, hyperdb.schema.dir)}')\n`
   str += '\n'
 
   const collections = []
@@ -236,4 +237,8 @@ function generateIndexKeyEncoding (type) {
   }
   str += `], { prefix: ${type.id} })`
   return str
+}
+
+function generateRequire (from, to) {
+  return path.relative(from, to).replace(/\\/g, '/') // unix paths for require
 }
